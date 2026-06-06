94a766e394c30daeebca918463c54cb005518182
mirza-developer <ali.qader3@gmail.com>
2026-06-06 14:42:16 +0330

    Move OrderTrackResult to new file and update namespace

--- PineSms.OrderTrack/Services/OrderTrackingService.cs
+++ PineSms.OrderTrack/Services/OrderTrackingService.cs
@@ -28,22 +28,3 @@ public class OrderTrackingService(HttpClient httpClient)
         }
     }
 }
-
-
-public class OrderTrackResult
-{
-    public bool Found { get; set; }
-    public string OrderCode { get; set; } = string.Empty;
-    public string StatusTitle { get; set; } = string.Empty;
-    public string? PostalTrackingCode { get; set; }
-    public DateTime UpdatedAt { get; set; }
-}
-
-[JsonSourceGenerationOptions(
-    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-    PropertyNameCaseInsensitive = true)]
-[JsonSerializable(typeof(OrderTrackResult))]
-public partial class OrderTrackResultContext : JsonSerializerContext
-{
-
-}
